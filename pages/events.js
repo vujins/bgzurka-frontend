5e7443ddb6ce2305bec66011fe74bf0c1a9a5515
Strahinja Vujin <strahinja.vujin@exelaonline.com>
2020-03-06 15:05:33 +0100

    moved map in events above

--- pages/events.js
+++ pages/events.js
@@ -5,38 +5,39 @@ import Map from '../components/Map';
 
 const events = props => {
   return (
-    <div className="row">
-      <div className="col-5">
-        <ul className="list-group">
-          {props &&
-            props.events &&
-            props.events.map(event => (
-              <Link href="/test" key={event.slug}>
-                <li
-                  className="list-group-item"
-                  key={event.slug}
-                  onClick={() => props.selectEvent(event)}
-                >
-                  <Event event={event} />
-                </li>
-              </Link>
-            ))}
-        </ul>
-      </div>
-      <div className="col-7">
-        <Map
-          markers={
-            props &&
-            props.events &&
-            props.events.map(event => ({
-              lat: event.location.coordinates[1],
-              lng: event.location.coordinates[0]
-            }))
-          }
-          zoom={12}
-        />
-      </div>
-    </div>
+    <>
+      {/* <div className="col-7"> */}
+      <Map
+        markers={
+          props &&
+          props.events &&
+          props.events.map(event => ({
+            lat: event.location.coordinates[1],
+            lng: event.location.coordinates[0]
+          }))
+        }
+        zoom={12}
+      />
+      {/* </div> */}
+
+      {/* <div className="col-5"> */}
+      <ul className="list-group mt-2">
+        {props &&
+          props.events &&
+          props.events.map(event => (
+            <Link href="/test" key={event.slug}>
+              <li
+                className="list-group-item"
+                key={event.slug}
+                onClick={() => props.selectEvent(event)}
+              >
+                <Event event={event} />
+              </li>
+            </Link>
+          ))}
+      </ul>
+      {/* </div> */}
+    </>
   );
 };
 
